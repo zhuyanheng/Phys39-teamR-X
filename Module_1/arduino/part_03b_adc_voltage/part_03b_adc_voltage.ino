9b6e109b7b321517b18c45827906353583ba6956
Xavier zhu <yanhengz@brandeis.edu>
2026-09-04 14:32:42 -0400

    Update part_03b_adc_voltage.ino

--- Module_1/arduino/part_03b_adc_voltage/part_03b_adc_voltage.ino
+++ Module_1/arduino/part_03b_adc_voltage/part_03b_adc_voltage.ino
@@ -1,18 +1,16 @@
-const int ANALOG_PIN = A0;
 const float V_REF_VOLTS = 5.00;
-const unsigned long REPORT_INTERVAL_MS = 100;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  const int adcCount = analogRead(ANALOG_PIN);
-  const float voltage = V_REF_VOLTS * adcCount / 1023.0;
+  int adcCount = analogRead(A0);
 
-  // Displaying four decimals does not increase the physical ADC resolution.
+  float voltage = V_REF_VOLTS * adcCount / 1023.0;
+
   Serial.print("Voltage_V:");
   Serial.println(voltage, 4);
 
-  delay(REPORT_INTERVAL_MS);
+  delay(100);
 }
