cb50a626cbcb4aaa84ab074e01211652865ae30f
Xavier zhu <yanhengz@brandeis.edu>
2026-09-04 14:29:59 -0400

    Update part_03a_adc_integer.ino

--- Module_1/arduino/part_03a_adc_integer/part_03a_adc_integer.ino
+++ Module_1/arduino/part_03a_adc_integer/part_03a_adc_integer.ino
@@ -1,16 +1,12 @@
-const int ANALOG_PIN = A0;
-const unsigned long REPORT_INTERVAL_MS = 100;
-
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  const int adcCount = analogRead(ANALOG_PIN);
+  int adcCount = analogRead(A0);
 
-  // Print one labeled integer quantity for Serial Monitor and Serial Plotter.
   Serial.print("ADC:");
   Serial.println(adcCount);
 
-  delay(REPORT_INTERVAL_MS);
+  delay(100);
 }
